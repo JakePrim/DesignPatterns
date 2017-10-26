7bbe4f8c8703e3c33ad35039eabd066bd8a975dd
sufulu <1760473022@qq.com>
2017-10-26 22:16:14 +0800

    原型模式学习完成

--- app/src/main/java/com/linksu/android_kotlin_designpatterns/prototypedesign/Client.java
+++ app/src/main/java/com/linksu/android_kotlin_designpatterns/prototypedesign/Client.java
@@ -12,24 +12,26 @@ import com.linksu.android_kotlin_designpatterns.prototypedesign.actualcombat.Use
 public class Client {
     public static void main(String[] args) throws CloneNotSupportedException {
 //        //原始文档
-//        WordDocument wordDocument = new WordDocument();
-//        wordDocument.setmText("这是正本文档");
-//        wordDocument.addImage("图片1");
-//        wordDocument.addImage("图片2");
-//        wordDocument.addImage("图片3");
-//        wordDocument.showDocument();
-//
-//        //已原始文档为原型 拷贝一个副本 注意：Cloneable 实现拷贝时，构造函数是不会执行的
-//        WordDocument clone = wordDocument.clone();
-//        clone.showDocument();
-//
-//        //修改副本文档
-//        clone.setmText("这是副本文档");
-//        clone.addImage("副本图片");
-//        clone.showDocument();
-//
-//        //不会影响原始文档
-//        wordDocument.showDocument();
+        WordDocument wordDocument = new WordDocument();
+        wordDocument.setmText("这是正本文档");
+        wordDocument.addImage("图片1");
+        wordDocument.addImage("图片2");
+        wordDocument.addImage("图片3");
+        wordDocument.showDocument();
+
+        //已原始文档为原型 拷贝一个副本 注意：Cloneable 实现拷贝时，构造函数是不会执行的
+        WordDocument clone = wordDocument.clone();
+        clone.showDocument();
+
+        //修改副本文档
+        clone.setmText("这是副本文档");
+        clone.addImage("副本图片");
+        clone.showDocument();
+
+        //不会影响原始文档
+        wordDocument.showDocument();
+
+
         LoginImpl login = new LoginImpl();
         login.login();
         User loginedUser = LoginSession.getLoginSession().getLoginedUser();
